539fad06a0f09617910b8df3ebf525a233e3ec6f
Obie <obiemunozjr@gmail.com>
2023-04-30 18:28:38 -0400

    Tokens placed in config so user can update. Added config reset.

--- src/extension.ts
+++ src/extension.ts
@@ -1,108 +1,9 @@
 import * as vscode from "vscode";
 
-const tokens: [string, number][] = [
-  ["$size-spacing-0", 0],
-  ["$size-spacing-1", 0.25],
-  ["$size-spacing-2", 0.5],
-  ["$size-spacing-3", 0.75],
-  ["$size-spacing-4", 1],
-  ["$size-spacing-5", 1.25],
-  ["$size-spacing-6", 1.5],
-  ["$size-spacing-7", 1.75],
-  ["$size-spacing-8", 2],
-  ["$size-spacing-9", 2.25],
-  ["$size-spacing-10", 2.5],
-  ["$size-spacing-11", 2.75],
-  ["$size-spacing-12", 3],
-  ["$size-spacing-14", 3.5],
-  ["$size-spacing-16", 4],
-  ["$size-spacing-20", 5],
-  ["$size-spacing-24", 6],
-  ["$size-spacing-28", 7],
-  ["$size-spacing-32", 8],
-  ["$size-spacing-36", 9],
-  ["$size-spacing-40", 10],
-  ["$size-spacing-44", 11],
-  ["$size-spacing-48", 12],
-  ["$size-spacing-52", 13],
-  ["$size-spacing-56", 14],
-  ["$size-spacing-60", 15],
-  ["$size-spacing-64", 16],
-  ["$size-spacing-72", 18],
-  ["$size-spacing-80", 20],
-  ["$size-spacing-96", 24],
-  ["$size-spacing-px", 0.0625],
-  ["$size-spacing-0-5", 0.125],
-  ["$breakpoint-md", 37.5],
-  ["$breakpoint-lg", 56.563],
-  ["$breakpoint-xl", 77.5],
-];
+const config = vscode.workspace.getConfiguration("design-system-linter");
 
-const colorTokens: [string, string][] = [
-  ["$color-base-gray-100", "#f2f2f2"],
-  ["$color-base-gray-200", "#e0e0e0"],
-  ["$color-base-gray-300", "#a8a8a8"],
-  ["$color-base-gray-400", "#8d8d8d"],
-  ["$color-base-gray-500", "#6f6f6f"],
-  ["$color-base-gray-600", "#525252"],
-  ["$color-base-gray-700", "#363534"],
-  ["$color-base-orange-300", "#f68c2c"],
-  ["$color-base-orange-900", "#bc5801"],
-  ["$color-base-white", "#ffffff"],
-  ["$color-border-base", "#363534"],
-  ["$color-border-focus", "#363534"],
-  ["$color-border-inverse-base", "#ffffff"],
-  ["$color-border-inverse-focus", "#ffffff"],
-  ["$color-border-button-outline", "#363534"],
-  ["$color-border-button-inverse-outline", "#ffffff"],
-  ["$color-brand-background", "#ffffff"],
-  ["$color-brand-primary-base", "#363534"],
-  ["$color-brand-primary-hover", "#525252"],
-  ["$color-brand-primary-active", "#6f6f6f"],
-  ["$color-brand-primary-disabled", "#a8a8a8"],
-  ["$color-brand-surface-base", "#f2f2f2"],
-  ["$color-brand-surface-hover", "#e0e0e0"],
-  ["$color-brand-surface-active", "#a8a8a8"],
-  ["$color-brand-surface-disabled", "#a8a8a8"],
-  ["$color-brand-on-primary", "#ffffff"],
-  ["$color-brand-on-surface", "#363534"],
-  ["$color-brand-on-background-base", "#363534"],
-  ["$color-brand-on-background-variant", "#6f6f6f"],
-  ["$color-brand-inverse-background", "#363534"],
-  ["$color-brand-inverse-primary-base", "#ffffff"],
-  ["$color-brand-inverse-primary-hover", "#f2f2f2"],
-  ["$color-brand-inverse-primary-active", "#e0e0e0"],
-  ["$color-brand-inverse-primary-disabled", "#a8a8a8"],
-  ["$color-brand-inverse-surface-base", "#525252"],
-  ["$color-brand-inverse-surface-hover", "#6f6f6f"],
-  ["$color-brand-inverse-surface-active", "#8d8d8d"],
-  ["$color-brand-inverse-surface-disabled", "#a8a8a8"],
-  ["$color-brand-inverse-on-primary", "#363534"],
-  ["$color-brand-inverse-on-surface", "#ffffff"],
-  ["$color-brand-inverse-on-background-base", "#ffffff"],
-  ["$color-brand-inverse-on-background-variant", "#a8a8a8"],
-  ["$color-brand-support-informational", "#363534"],
-  ["$color-brand-support-error", "#f4d8da"],
-  ["$color-brand-support-warning", "#ffdd99"],
-  ["$color-brand-support-success", "#000000"],
-  ["$color-brand-support-on-informational", "#ffffff"],
-  ["$color-brand-support-on-error", "#b10009"],
-  ["$color-brand-support-on-warning", "#363534"],
-  ["$color-brand-support-on-success", "#000000"],
-  ["$color-font-button-primary", "#ffffff"],
-  ["$color-font-button-outline", "#363534"],
-  ["$color-font-button-ghost", "#363534"],
-  ["$color-font-button-disabled", "#363534"],
-  ["$color-font-button-inverse-primary", "#363534"],
-  ["$color-font-button-inverse-outline", "#ffffff"],
-  ["$color-font-button-inverse-ghost", "#ffffff"],
-  ["$color-support-error-base", "#b10009"],
-  ["$color-support-error-light", "#f4d8da"],
-  ["$color-support-warning-base", "#ffdd99"],
-  ["$color-support-warning-light", "#ffeecc"],
-  ["$color-support-success-base", "#006600"],
-  ["$color-support-success-light", "#d8ecb6"],
-];
+const tokens = config.get<[string, number][]>("defaultSpacingTokens") || [];
+const colorTokens = config.get<[string, string][]>("defaultColorTokens") || [];
 
 let isEnabled = true;
 let statusBarItem: vscode.StatusBarItem;
@@ -271,6 +172,7 @@ function handleSpacingValue(
     diagnostics.push(
       new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Warning)
     );
+
     const decoration: vscode.DecorationOptions = {
       range,
       renderOptions: {
@@ -435,6 +337,16 @@ export function activate(context: vscode.ExtensionContext): void {
     lintDocument(vscode.window.activeTextEditor.document);
   }
 
+  // Register the reset configuration command
+  let resetConfigurationDisposable = vscode.commands.registerCommand(
+    "design-system-linter.resetConfiguration",
+    () => {
+      resetConfiguration();
+    }
+  );
+
+  context.subscriptions.push(resetConfigurationDisposable);
+
   context.subscriptions.push(
     vscode.window.onDidChangeActiveTextEditor((editor) => {
       if (editor && isEnabled) {
@@ -464,6 +376,37 @@ export function activate(context: vscode.ExtensionContext): void {
       diagnosticCollection.delete(document.uri);
     })
   );
+}
+
+/**
+ * @description This function will reset the configuration to the default values
+ * @returns {void}
+ */
+async function resetConfiguration(): Promise<void> {
+  const config = vscode.workspace.getConfiguration("design-system-linter");
+
+  config.update(
+    "defaultSpacingTokens",
+    undefined,
+    vscode.ConfigurationTarget.Global
+  );
+  config.update(
+    "defaultColorTokens",
+    undefined,
+    vscode.ConfigurationTarget.Global
+  );
+
+  // Show a message to inform the user that the configuration has been reset and ask them to reload the window
+  const reloadAction = "Reload Window";
+  const result = await vscode.window.showInformationMessage(
+    "Design System Linter configuration has been reset to defaults. Please reload the window to apply the changes.",
+    reloadAction
+  );
+
+  // If the user clicked the "Reload Window" button, execute the "workbench.action.reloadWindow" command
+  if (result === reloadAction) {
+    vscode.commands.executeCommand("workbench.action.reloadWindow");
+  }
 }
 
 export function deactivate() {
