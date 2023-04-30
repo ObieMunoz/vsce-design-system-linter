4e4ec557f77e4a5a4b7339ea725f9f420ab3f4bc
Obie <obiemunozjr@gmail.com>
2023-04-30 10:40:11 -0400

    Add colorTokens; Split functionality for colors & spacing

--- src/extension.ts
+++ src/extension.ts
@@ -38,6 +38,72 @@ const tokens: [string, number][] = [
   ["$breakpoint-xl", 77.5],
 ];
 
+const colorTokens: [string, string][] = [
+  ["$color-base-gray-100", "#f2f2f2"],
+  ["$color-base-gray-200", "#e0e0e0"],
+  ["$color-base-gray-300", "#a8a8a8"],
+  ["$color-base-gray-400", "#8d8d8d"],
+  ["$color-base-gray-500", "#6f6f6f"],
+  ["$color-base-gray-600", "#525252"],
+  ["$color-base-gray-700", "#363534"],
+  ["$color-base-orange-300", "#f68c2c"],
+  ["$color-base-orange-900", "#bc5801"],
+  ["$color-base-white", "#ffffff"],
+  ["$color-border-base", "#363534"],
+  ["$color-border-focus", "#363534"],
+  ["$color-border-inverse-base", "#ffffff"],
+  ["$color-border-inverse-focus", "#ffffff"],
+  ["$color-border-button-outline", "#363534"],
+  ["$color-border-button-inverse-outline", "#ffffff"],
+  ["$color-brand-background", "#ffffff"],
+  ["$color-brand-primary-base", "#363534"],
+  ["$color-brand-primary-hover", "#525252"],
+  ["$color-brand-primary-active", "#6f6f6f"],
+  ["$color-brand-primary-disabled", "#a8a8a8"],
+  ["$color-brand-surface-base", "#f2f2f2"],
+  ["$color-brand-surface-hover", "#e0e0e0"],
+  ["$color-brand-surface-active", "#a8a8a8"],
+  ["$color-brand-surface-disabled", "#a8a8a8"],
+  ["$color-brand-on-primary", "#ffffff"],
+  ["$color-brand-on-surface", "#363534"],
+  ["$color-brand-on-background-base", "#363534"],
+  ["$color-brand-on-background-variant", "#6f6f6f"],
+  ["$color-brand-inverse-background", "#363534"],
+  ["$color-brand-inverse-primary-base", "#ffffff"],
+  ["$color-brand-inverse-primary-hover", "#f2f2f2"],
+  ["$color-brand-inverse-primary-active", "#e0e0e0"],
+  ["$color-brand-inverse-primary-disabled", "#a8a8a8"],
+  ["$color-brand-inverse-surface-base", "#525252"],
+  ["$color-brand-inverse-surface-hover", "#6f6f6f"],
+  ["$color-brand-inverse-surface-active", "#8d8d8d"],
+  ["$color-brand-inverse-surface-disabled", "#a8a8a8"],
+  ["$color-brand-inverse-on-primary", "#363534"],
+  ["$color-brand-inverse-on-surface", "#ffffff"],
+  ["$color-brand-inverse-on-background-base", "#ffffff"],
+  ["$color-brand-inverse-on-background-variant", "#a8a8a8"],
+  ["$color-brand-support-informational", "#363534"],
+  ["$color-brand-support-error", "#f4d8da"],
+  ["$color-brand-support-warning", "#ffdd99"],
+  ["$color-brand-support-success", "#000000"],
+  ["$color-brand-support-on-informational", "#ffffff"],
+  ["$color-brand-support-on-error", "#b10009"],
+  ["$color-brand-support-on-warning", "#363534"],
+  ["$color-brand-support-on-success", "#000000"],
+  ["$color-font-button-primary", "#ffffff"],
+  ["$color-font-button-outline", "#363534"],
+  ["$color-font-button-ghost", "#363534"],
+  ["$color-font-button-disabled", "#363534"],
+  ["$color-font-button-inverse-primary", "#363534"],
+  ["$color-font-button-inverse-outline", "#ffffff"],
+  ["$color-font-button-inverse-ghost", "#ffffff"],
+  ["$color-support-error-base", "#b10009"],
+  ["$color-support-error-light", "#f4d8da"],
+  ["$color-support-warning-base", "#ffdd99"],
+  ["$color-support-warning-light", "#ffeecc"],
+  ["$color-support-success-base", "#006600"],
+  ["$color-support-success-light", "#d8ecb6"],
+];
+
 let isEnabled = true;
 let statusBarItem: vscode.StatusBarItem;
 
@@ -79,6 +145,82 @@ function findNearestToken(value: number): string {
   return nearestToken;
 }
 
+function findExactToken(hexColor: string): string | undefined {
+  for (const [token, tokenColor] of colorTokens) {
+    if (tokenColor.toLowerCase() === hexColor.toLowerCase()) {
+      return token;
+    }
+  }
+  return undefined;
+}
+
+function handleSpacingValue(
+  match: RegExpExecArray,
+  document: vscode.TextDocument,
+  diagnostics: vscode.Diagnostic[],
+  decorations: vscode.DecorationOptions[]
+) {
+  const pxValueString = match[2].trim();
+  const pxValues = pxValueString.match(/(\d+)px/g) || [];
+
+  pxValues.forEach((pxValue) => {
+    const value = parseInt(pxValue, 10);
+    const startPosition = document.positionAt(
+      (match.index ?? 0) + match[0].indexOf(pxValue)
+    );
+    const endPosition = startPosition.translate(0, pxValue.length);
+    const range = new vscode.Range(startPosition, endPosition);
+
+    const recommendation = findNearestToken(value / 16);
+    const message = `DESIGN SYSTEM: Consider using '${recommendation}' instead of '${pxValue}'.`;
+
+    diagnostics.push(
+      new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Warning)
+    );
+    const decoration: vscode.DecorationOptions = {
+      range,
+      renderOptions: {
+        after: {
+          contentText: ` ⟶ ${recommendation}`,
+        },
+      },
+    };
+
+    decorations.push(decoration);
+  });
+}
+
+function handleColorValue(
+  match: RegExpExecArray,
+  document: vscode.TextDocument,
+  diagnostics: vscode.Diagnostic[],
+  decorations: vscode.DecorationOptions[]
+) {
+  const colorValue = match[2].trim();
+  const startPosition = document.positionAt(match.index ?? 0);
+  const endPosition = startPosition.translate(0, match[0].length);
+  const range = new vscode.Range(startPosition, endPosition);
+
+  const recommendation = findExactToken(colorValue);
+  if (recommendation) {
+    const message = `DESIGN SYSTEM: Consider using '${recommendation}' instead of '${colorValue}'.`;
+
+    diagnostics.push(
+      new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Warning)
+    );
+    const decoration: vscode.DecorationOptions = {
+      range,
+      renderOptions: {
+        after: {
+          contentText: ` ⟶ ${recommendation}`,
+        },
+      },
+    };
+
+    decorations.push(decoration);
+  }
+}
+
 let diagnosticCollection: vscode.DiagnosticCollection;
 
 const recommendationDecorationType =
@@ -105,38 +247,23 @@ function lintDocument(document: vscode.TextDocument) {
   const decorations: vscode.DecorationOptions[] = [];
 
   const text = document.getText();
-  const regex = /([\w-]+)\s*:\s*([\d\s]*(?:\d+px\b\s*)+)/g;
+
+  // Match spacing values in px
+  const spacingRegex = /([\w-]+)\s*:\s*([\d\s]*(?:\d+px\b\s*)+)/g;
+  // Match hex and rgba color values
+  const colorRegex =
+    /([\w-]+)\s*:\s*(#[0-9a-fA-F]{3,8}|rgb\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*(?:,\s*[\d.]+)?\s*\))/g;
+
   let match: RegExpExecArray | null;
 
-  while ((match = regex.exec(text)) !== null) {
-    const pxValueString = match[2].trim();
-    const pxValues = pxValueString.match(/(\d+)px/g) || [];
+  // Handle spacing values
+  while ((match = spacingRegex.exec(text)) !== null) {
+    handleSpacingValue(match, document, diagnostics, decorations);
+  }
 
-    pxValues.forEach((pxValue) => {
-      const value = parseInt(pxValue, 10);
-      const startPosition = document.positionAt(
-        (match?.index ?? 0) + (match ? match[0].indexOf(pxValue) : 0)
-      );
-      const endPosition = startPosition.translate(0, pxValue.length);
-      const range = new vscode.Range(startPosition, endPosition);
-
-      const recommendation = findNearestToken(value / 16);
-      const message = `Consider using '${recommendation}' instead of '${pxValue}'.`;
-
-      diagnostics.push(
-        new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Warning)
-      );
-      const decoration: vscode.DecorationOptions = {
-        range,
-        renderOptions: {
-          after: {
-            contentText: ` ⟶ ${recommendation}`,
-          },
-        },
-      };
-
-      decorations.push(decoration);
-    });
+  // Handle color values
+  while ((match = colorRegex.exec(text)) !== null) {
+    handleColorValue(match, document, diagnostics, decorations);
   }
 
   diagnosticCollection.set(document.uri, diagnostics);
